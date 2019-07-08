fc1573d76872a6cf952a0ec985d2f0c5667d7c5c
“daniellimhong” <daniellimhong@gmail.com>
2019-07-08 16:51:05 -0700

    first commit

---
+++ src/components/stateless/Background1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "./Background1.css";
+
+export default class Background1 extends Component {
+    render(){
+        return(
+        
+            <img className="Background1" 
+            src="https://cdn.pixabay.com/photo/2015/09/05/20/02/coding-924920_960_720.jpg"
+            alt=""/> 
+        )
+    }
+}
